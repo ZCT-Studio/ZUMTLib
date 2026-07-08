10bb23aeb1db416f2da28ab6895ff2fbafd9430f
wanjiangzhi <wanjiangzhi@outlook.com>
2026-07-08 22:23:15 +0800

    转交

--- ZUMTLib/ZUMTLib_Cfg.h
+++ ZUMTLib/ZUMTLib_Cfg.h
@@ -1,23 +1,4 @@
-//     ZUMTLib - A C++ library for Unix Memory Operations
-//     Copyright (C) 2026  CZF-H
-//
-//     This library is free software; you can redistribute it and/or
-//     modify it under the terms of the GNU Lesser General Public
-//     License as published by the Free Software Foundation; either
-//     version 2.1 of the License, or (at your option) any later version.
-//
-//     This library is distributed in the hope that it will be useful,
-//     but WITHOUT ANY WARRANTY; without even the implied warranty of
-//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-//     Lesser General Public License for more details.
-//
-//     You should have received a copy of the GNU Lesser General Public
-//     License along with this library; if not, write to the Free Software
-//     Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
-//     USA
-//
-//     江芷酱紫, 24 March 2026, Authorized Contributor of CZF-H
-//     Contact: wanjiangzhi@outlook.com
+// Licensed under the GNU Lesser General Public License, Version 2.1
 
 //
 // Created by wanjiangzhi on 2026/3/24.
